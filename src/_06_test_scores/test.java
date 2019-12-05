f48c8536b22468206f5b85d884cd9a117bc8274f
ilabstaff <ilabstaff@172.16.116.68>
2019-12-04 17:53:39 -0800

    test_score

---
+++ src/_06_test_scores/test.java
@@ -0,0 +1,16 @@
+package _06_test_scores;
+
+import javax.swing.JOptionPane;
+
+public class test {
+	public static void main(String[] args) {
+		
+		String input=JOptionPane.showInputDialog("What was your tesst score");
+double score=Double.parseDouble(input);
+	}
+	if(score==80) 
+		Wow();
+	
+			
+
+}
